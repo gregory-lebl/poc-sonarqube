40d67d0ccfb28c8fc3bebf5dbcc8698844254fc6
Gregory <gregory.leblond.dev@gmail.com>
2024-09-28 21:19:30 +0200

    [FEATURE] Add files

---
+++ test-app/app/page.tsx
@@ -0,0 +1,5 @@
+import {Chart} from "@/components/Chart";
+
+export default function Home() {
+    return <Chart/>
+}
---
+++ test-app/tests/default.test.ts
@@ -0,0 +1,5 @@
+describe('Default test suite', () => {
+    test('Default test', () => {
+        expect(false).toBeTruthy()
+    });
+});
